000c23e5d1de07e48cb24e8ef01742fe6455fc8b
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2025-05-28 18:09:54 +0200

    Add parsers for Kaspersky AV

---
+++ maldump/collectors/kaspersky/kaspersky_fielsystem_parser.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import logging
+
+from maldump.collectors.building_block import BuildingBlock
+from maldump.collectors.parser import Parser
+from maldump.constants import ThreatMetadata
+from maldump.structures import QuarEntry
+from maldump.utils import DatetimeConverter as DTC
+from maldump.utils import Parser as parse
+
+
+logger = logging.getLogger(__name__)
+
+
+class KasperskyFilesystemParser(Parser):
+
+    @BuildingBlock._comp_wrapper
+    def compute(self) -> list[QuarEntry]:
+        logger.info("Parsing from filesystem in %s", self.__class__.__name__)
+        quarfiles = []
+
+        for idx, entry in enumerate(self.path.glob("{*}")):
+            logger.debug('Parsing entry, idx %s, path "%s"', idx, entry)
+            if not entry.is_file():
+                logger.debug("Entry (idx %s) is not a file, skipping", idx)
+                continue
+
+            # TODO:
+            # filename = entry.name
+            # filename = entry.name
+            #
+            # if filename in data:
+            #     logger.debug("Entry (idx %s) already found, skipping", idx)
+            #     continue
+
+            # malfile = self._get_malfile(filename)
+
+            entry_stat = parse(self).entry_stat(entry)
+            if entry_stat is None:
+                logger.debug('Skipping entry idx %s, path "%s"', idx, entry)
+                continue
+            timestamp = DTC.get_dt_from_stat(entry_stat)
+            size = entry_stat.st_size
+
+            q = QuarEntry(self)
+            q.path = str(entry)
+            q.timestamp = timestamp
+            q.size = size
+            q.threat = ThreatMetadata.UNKNOWN_THREAT
+            # q.malfile = malfile
+            # quarfiles[filename] = q
+            quarfiles.append(q)
+
+        return quarfiles
---
+++ maldump/collectors/kaspersky/kaspersky_metadata_parser.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+import logging
+import sqlite3
+from datetime import datetime
+
+from maldump.collectors.building_block import BuildingBlock
+from maldump.collectors.parser import Parser
+from maldump.structures import QuarEntry
+
+from maldump.utils import Logger as log
+
+
+logger = logging.getLogger(__name__)
+
+
+class KasperskyMetadataParser(Parser):
+    @log.log(lgr=logger)
+    def _normalize_time(self, number: int) -> datetime:
+        year = (number >> 48) & 0xFFFF
+        month = (number >> 40) & 0xFF
+        days = (number >> 32) & 0xFF
+        hours = (number >> 24) & 0xFF
+        minutes = (number >> 16) & 0xFF
+        seconds = (number >> 8) & 0xFF
+
+        return datetime(year, month, days, hours, minutes, seconds)
+
+    @BuildingBlock._comp_wrapper
+    def compute(self) -> list[QuarEntry]:
+        logger.info("Parsing from log in %s", self.__class__.__name__)
+        quarfiles = []
+
+        db_file = self.path.joinpath("quarantine.db").resolve()
+        try:
+            logger.debug(
+                'Trying to open and read from database file, path "%s"', db_file
+            )
+            conn = sqlite3.connect(db_file)
+            logger.debug('Opening cursor to a database connection, path "%s"', db_file)
+            cursor = conn.cursor()
+            logger.debug(
+                'Exectuting a command with a database connection, path "%s"', db_file
+            )
+            cursor.execute("SELECT * FROM 'objects'")
+            rows = cursor.fetchall()
+        except sqlite3.Error as e:
+            logger.exception(
+                'Cannot open nor read from a database file, path "%s"',
+                db_file,
+                exc_info=e,
+            )
+            return quarfiles
+
+        for row in rows:
+            # filename = row[0]
+            # TODO:
+            # malfile = self._get_malfile(filename)
+            q = QuarEntry(self)
+            q.timestamp = self._normalize_time(row[6])
+            q.threat = row[3]
+            q.path = row[1] + row[2]
+            q.size = row[7]
+            # q.malfile = malfile
+            # quarfiles[filename] = q
+            quarfiles.append(q)
+
+        conn.close()
+
+        return quarfiles
