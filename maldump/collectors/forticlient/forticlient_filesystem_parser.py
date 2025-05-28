b2868251455c7119c6612840d0ab7e7f85c324ed
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2025-05-28 18:09:23 +0200

    Add parsers for Forticlient AV

---
+++ maldump/collectors/forticlient/forticlient_filesystem_parser.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+import logging
+
+from maldump.collectors.building_block import BuildingBlock
+from maldump.collectors.parser import Parser
+from maldump.structures import QuarEntry
+from maldump.utils import Parser as parse
+from maldump.utils import Logger as log
+from maldump.parsers.kaitai.forticlient_parser import ForticlientParser
+
+logger = logging.getLogger(__name__)
+
+
+class ForticlientFilesystemParser(Parser):
+
+    @log.log(lgr=logger)
+    def _normalize_path(self, path):
+        if path[2:4] == "?\\":
+            path = path[4:]
+        return path
+
+    @log.log(lgr=logger)
+    def _get_time(self, ts: ForticlientParser.Timestamp):
+        return datetime(ts.year, ts.month, ts.day, ts.hour, ts.minute, ts.second)
+
+    @BuildingBlock._comp_wrapper
+    def compute(self) -> list[QuarEntry]:
+        logger.info("Parsing from log in %s", self.__class__.__name__)
+        quarfiles = []
+
+        for idx, metafile in enumerate(self.path.glob("*[!.meta]")):
+            logger.debug('Parsing entry, idx %s, path "%s"', idx, metafile)
+
+            kt = parse(self).kaitai(ForticlientParser, metafile)
+            if kt is None:
+                logger.debug('Skipping entry idx %s, path "%s"', idx, metafile)
+                continue
+
+            q = QuarEntry(self)
+            q.timestamp = self._get_time(kt.timestamp)
+            q.threat = kt.mal_type
+            q.path = self._normalize_path(kt.mal_path)
+            q.size = kt.mal_len
+            # TODO
+            # q.malfile = kt.mal_file
+            # quarfiles[str(metafile)] = q
+            quarfiles.append(q)
+            kt.close()
+
+        return quarfiles
